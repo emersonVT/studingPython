d015738f77d78c84f075fc5e4406fd259f2d45cd
Emerson Virgílio Tomo <emersongabrieltomo@gmail.com>
2026-01-04 02:01:30 +0200

    Create caesarCipher.py

---
+++ caesarCipher.py
@@ -0,0 +1,10 @@
+# Building a Caesar Cipher
+def caesar(text,shift):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table= str.maketrans(alphabet,shifted_alphabet)
+    encrypted_text = text.translate(translation_table)
+    print(encrypted_text)
+
+encrypted_text = caesar("freeCodeCamp",3)
+print(encrypted_text)
