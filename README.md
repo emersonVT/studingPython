d2c12aa05e4a9a56562ec999285989784f1dc3b5
Emerson Virgílio Tomo <emersongabrieltomo@gmail.com>
2026-01-06 23:19:26 +0200

    Add notes and examples about Python variables

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # studingPython
 I'm learning Python to build a system later on.
+AND
+This repository contains my learning progress while following FreeCodeCamp courses.
---
+++ python basics/variables-and-data-types/variables.py
@@ -0,0 +1,26 @@
+# Declaring variables
+name = "Emerson Tomo"
+age = 25
+
+#In Python, you can use the print function to print data to the terminal.
+print(name)
+print(age)
+
+# Variables are case-sensitive
+# If I use uppercase or lowercase in a variable with the same name, python can feel the letter and treat it  as different name.
+Age = 30
+AGE = 35
+
+print(Age)
+print(AGE)
+
+# Using descriptive variable names
+user_age = 25
+user_name = "Emerson"
+
+print(user_name)
+print(user_age)
+
+# Updating a variable
+user_age = user_age + 1
+print(user_age)
